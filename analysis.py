e0d85383f91a7f20b8819f9bd5996a518f27f433
Oliver-Molina <98718412+Oliver-Molina@users.noreply.github.com>
2025-04-18 17:13:46 -0600

    Expanding analysis verbosity

--- analysis.py
+++ analysis.py
@@ -16,31 +16,31 @@ class CardEntry:
         self.name = name
         self.is_missing = is_missing
         self.current_combo_ids = []
-        self.potential_combo_ids = []
-        self.current_combo_summaries = []
-        self.potential_combo_sumamries = []
+        self.combos_missing_another_card_ids = []
+        self.combos_missing_this_card_ids = []
         self.current_combo_results = []
-        self.potential_combo_results = []
+        self.combos_missing_another_card_results = []
+        self.combos_missing_this_card_results = []
         self.average_cards_per_current_combo = 0
         self.average_cards_per_all_combo = 0
 
     def add_combo_info(self, identifier, info:dict):
         if len(info["missing_cards"]) > 0:
-            self.potential_combo_ids.append(identifier)
-            combo_summary = " + ".join(info["required_cards"]).strip()
-            self.potential_combo_sumamries.append(combo_summary)
+            if self.name in info["missing_cards"]:
+                 self.combos_missing_this_card_ids.append(identifier)
             for result in info["results"]:
-                if result not in self.current_combo_results and result not in self.potential_combo_results:
-                    self.potential_combo_results.append(result) 
+                if result not in self.combos_missing_this_card_results:
+                    self.combos_missing_this_card_results.append(result) 
+            else:
+                self.combos_missing_another_card_ids.append(identifier)
+                for result in info["results"]:
+                    if result not in self.combos_missing_another_card_results:
+                        self.combos_missing_another_card_results.append(result) 
         else:
             self.current_combo_ids.append(identifier)
-            combo_summary = " + ".join(info["required_cards"]).strip()
-            self.current_combo_summaries.append(combo_summary)
             for result in info["results"]:
                 if result not in self.current_combo_results:
                     self.current_combo_results.append(result) 
-                if result in self.potential_combo_results:
-                    self.potential_combo_results.remove(result)
 
             current_average = self.average_cards_per_current_combo
             current_count = len(self.current_combo_ids) - 1
@@ -48,7 +48,7 @@ class CardEntry:
             self.average_cards_per_current_combo = new_average
 
         current_average = self.average_cards_per_all_combo
-        current_count = len(self.current_combo_ids) + len(self.potential_combo_ids) - 1
+        current_count = len(self.current_combo_ids) + len(self.combos_missing_another_card_ids) + len(self.combos_missing_this_card_ids) - 1
         new_average = (current_average * current_count + len(info["required_cards"])) / (current_count + 1)
         self.average_cards_per_all_combo = new_average
 
@@ -57,16 +57,21 @@ class CardEntry:
         missing_tag = " - Missing" if self.is_missing else ""
         print(f"CardName: {self.name}{missing_tag}")
         if not self.is_missing:
-            print(f"CurrentComboCount: {len(self.current_combo_ids)}")
-            print(f"AverageComboCardCountCurrent: {self.average_cards_per_current_combo}")
-            print(f"CurrentComboResults:")
+            print(f"CurrentCombos: {len(self.current_combo_ids)}")
+            print(f"AverageCardCountPerCurrentCombo: {self.average_cards_per_current_combo}")
+            print(f"Results:")
             for result in self.current_combo_results:
                 print(f" - {result}")
-
-        print(f"PotentialComboCount: {len(self.potential_combo_ids)}")
+        
         print(f"AverageComboCardCountAll: {self.average_cards_per_all_combo}")
-        print(f"PotentialComboResults:")
-        for result in self.potential_combo_results:
+        print(f"CombosMissingThisCard: {len(self.combos_missing_this_card_ids)}")
+        print(f"Results:")
+        for result in self.combos_missing_this_card_results:
+            print(f" - {result}")
+
+        print(f"CombosMissingAnotherCard: {len(self.combos_missing_another_card_ids)}")
+        print(f"Results:")
+        for result in self.combos_missing_another_card_results:
             print(f" - {result}")
 
 
@@ -91,6 +96,39 @@ def print_statistics(card_data: list[CardEntry]):
     for card in card_data:
         card.print_statistics()
 
+
+def print_card_combo(combo):
+    summary = ", ".join(combo["required_cards"] + combo["missing_cards"]).strip()
+    print(f"{summary}:")
+    print("prerequisites")
+    for prerequisite in combo["prerequisites"]:
+        print(f" - {prerequisite}")
+
+    print("steps:")
+    for step in combo["steps"]:
+        print(f" - {step}")
+
+    print("results:")
+    for result in combo["results"]:
+        print(f" - {result}")
+
+def print_card_combos(card:CardEntry, current_combos, potential_combos):
+    print(f"{card.name}:")
+    print("Current Combos:")
+    for combo_id in card.current_combo_ids:
+        combo = current_combos[combo_id]
+        print_card_combo(combo)
+
+    print("Combos missing this card:")
+    for combo_id in card.combos_missing_this_card_ids:
+        combo = potential_combos[combo_id]
+        print_card_combo(combo)
+
+    print("Combos missing another card:")
+    for combo_id in card.combos_missing_another_card_ids:
+        combo = potential_combos[combo_id]
+        print_card_combo(combo)
+
 def main():
     # Load combos
     with open(combos_filename, "r") as f:
@@ -108,12 +146,20 @@ def main():
 
     # Apply desired sorting conditions
     cards = list(combo_data.values())
-    cards = sorted(cards, key=lambda card: len(card.potential_combo_ids), reverse=True)
+    cards = sorted(cards, key=lambda card: len(card.combos_missing_another_card_ids), reverse=False)
     cards = sorted(cards, key=lambda card: len(card.current_combo_ids), reverse=False)
+    cards = sorted(cards, key=lambda card: len(card.combos_missing_this_card_ids), reverse=True)
+    # cards = sorted(cards, key=lambda card: card.is_missing, reverse=False)
     cards = [card for card in cards if card.is_missing == True]
+    #cards = [card for card in cards if len(card.current_combo_ids) == 1]
 
     # Display Results
     print_statistics(cards)
+    # for card in cards:
+    #     print_card_combos(card, current_combos, potential_combos)
+    #     print("")
+
+
 
 if __name__ == "__main__":
     main()
